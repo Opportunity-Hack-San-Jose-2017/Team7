c7c1e3334281fff0e1fb99f2a23ba1cfc21e28cc
Tran Tu <tratu@cisco.com>
2017-10-30 10:01:22 -0700

    ported states and responses to python

--- smsbot/smsbot/bin/web_app.py
+++ smsbot/smsbot/bin/web_app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import requests
 from flask import Flask, request
 
-
+states = {}
 log = logging.getLogger(__name__)
 
 
@@ -20,10 +20,90 @@ class FacebookConfig:
     VERIFY_TOKEN = 'moo'
     PAGE_ACCESS_TOKEN = 'EAARCCMNIDsQBAIqhyLHcQ2OaJlJtlXQgeDiug3Itk9HAYeZASZBhygKQ8SNf3ZC67wQ2vYqVg7zKErCCCvapLeShB6vD1c0gyNZBl1MLbFvItYcos6UwFZAKTBeaqcdpNMDoiYZCmASZAZCstCawyaUweZBKK1usFKhDCuUYnJ7e9QQZDZD'
 
+def handle_message(sender_id, received_message):
+    """
+    Decide what to do with this msg
+    """
+    response = {
+        "text": "I don't get it"
+    }
 
-def send_message(recipient_id, message_text):
+    if sender_id in states:
+        log.info("detected repeat user")
+        states[sender_id] += 1
+    else:
+        log.info("detected new user")
+        states[sender_id] = 1
 
-    log.info("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    # if received_message.attachments and received_message.attachments[0].type == "location":
+    #     log.info("detected location:" + received_message.attachments[0].payload)
+
+    if states[sender_id] == 1:
+        response = {
+            "text": "Are you in immediate danger?",
+            "quick_replies": [
+                {
+                    "content_type": "text",
+                    "title": "Yes",
+                    "payload": "reply1"
+                },
+                {
+                    "content_type": "text",
+                    "title": "No",
+                    "payload": "reply2"
+                }
+            ]
+        }
+    elif states[sender_id] == 2:
+        response = {
+            "text": "What is your location?",
+            "quick_replies": [
+                {
+                    "content_type":"location",
+                    "title": "location",
+                    "payload": "reply3"
+                }
+            ]
+            }
+    elif states[sender_id] == 3:
+        response = {
+            "text": "Select a category",
+            "quick_replies": [
+                {
+                    "content_type": "text",
+                    "title": "Violence",
+                    "payload": "reply1"
+                },
+                {
+                    "content_type": "text",
+                    "title": "Harassment",
+                    "payload": "reply2"
+                },
+                {
+                    "content_type": "text",
+                    "title": "Danger",
+                    "payload": "reply3"
+                },
+                {
+                    "content_type": "text",
+                    "title": "Other",
+                    "payload": "reply4"
+                }
+            ]
+        }
+    elif states[sender_id] == 4:
+        response = {
+            "text": "all done!"
+        }
+
+    send_response(sender_id, response)
+
+def send_response(recipient_id, response):
+    """
+    Have the bot reply to user
+    """
+
+    log.info("sending message to {recipient}: {text}".format(recipient=recipient_id, text=response["text"]))
 
     params = {
         "access_token": FacebookConfig.PAGE_ACCESS_TOKEN
@@ -35,9 +115,7 @@ def send_message(recipient_id, message_text):
         "recipient": {
             "id": recipient_id
         },
-        "message": {
-            "text": message_text
-        }
+        "message": response
     })
     r = requests.post(FacebookConfig.FACEBOOK_MESSAGEING_API, params=params, headers=headers, data=data)
     if r.status_code != 200:
@@ -45,7 +123,7 @@ def send_message(recipient_id, message_text):
         log.info(r.text)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/webhook', methods=['GET'])
 def facebook_verify():
     """
     When the endpoint is registered as a webhook, it must echo back
@@ -60,7 +138,7 @@ def facebook_verify():
     return "Hello world", 200
 
 
-@app.route('/', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def facebook_webhook():
     """
     Endpoint for processing incoming messaging events
@@ -75,19 +153,25 @@ def facebook_webhook():
 
                 if messaging_event.get("message"):  # someone sent us a message
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                    # the facebook ID of the person sending you the message
+                    sender_id = messaging_event["sender"]["id"]
+                    # the recipient's ID, which should be your page's facebook ID
+                    recipient_id = messaging_event["recipient"]["id"]
+                    # the message's text
+                    message_text = messaging_event["message"]["text"]
 
-                    send_message(sender_id, "roger that!")
+                    handle_message(sender_id, message_text)
 
-                if messaging_event.get("delivery"):  # delivery confirmation
+                # delivery confirmation
+                if messaging_event.get("delivery"):
                     pass
 
-                if messaging_event.get("optin"):  # optin confirmation
+                # optin confirmation
+                if messaging_event.get("optin"):
                     pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                # user clicked/tapped "postback" button in earlier message
+                if messaging_event.get("postback"):
                     pass
 
     return "ok", 200
