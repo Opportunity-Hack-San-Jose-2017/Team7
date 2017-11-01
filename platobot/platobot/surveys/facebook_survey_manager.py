95df8f90c9b248b3e2ae8dfa7bff7cd043e0f433
Tran Tu <tratu@cisco.com>
2017-11-01 12:43:32 -0700

    move send response to facebook_api

--- platobot/platobot/surveys/facebook_survey_manager.py
+++ platobot/platobot/surveys/facebook_survey_manager.py
@@ -1,14 +1,11 @@
 import yaml
 import os
 import logging
-import json
-import requests
 
-from platobot.config import FacebookConfig
 from platobot.surveys.survey_manager import SurveyManager
+from platobot.utils.facebook_api import send_response
 
 log = logging.getLogger(__name__)
-
 
 class FacebookSurveyManager(SurveyManager):
 
@@ -25,7 +22,7 @@ class FacebookSurveyManager(SurveyManager):
             survey_record.state = -1
             response = self.complete_msg
         finally:
-            self.send_response(survey_record.user, response)
+            send_response(survey_record.user, response)
 
     def get_survey_specs(self):
         return yaml.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "facebook/violence.yaml")))
@@ -33,26 +30,3 @@ class FacebookSurveyManager(SurveyManager):
     def get_generic_survey_fields(self):
         return self.generic_survey_fields
 
-    def send_response(self, recipient_id, response):
-        """
-        Have the bot reply to user
-        """
-
-        log.info("sending message to {recipient}: {text}".format(recipient=recipient_id, text=response["text"]))
-
-        params = {
-            "access_token": FacebookConfig.PAGE_ACCESS_TOKEN
-        }
-        headers = {
-            "Content-Type": "application/json"
-        }
-        data = json.dumps({
-            "recipient": {
-                "id": recipient_id
-            },
-            "message": response
-        })
-        r = requests.post(FacebookConfig.FACEBOOK_MESSAGEING_API, params=params, headers=headers, data=data)
-        if r.status_code != 200:
-            log.info(r.status_code)
-            log.info(r.text)
---
+++ platobot/platobot/utils/facebook_api.py
@@ -0,0 +1,31 @@
+import logging
+import json
+import requests
+
+from platobot.config import FacebookConfig
+
+log = logging.getLogger(__name__)
+
+def send_response(recipient_id, response):
+    """
+    Have the bot reply to user
+    """
+
+    log.info("sending message to {recipient}: {text}".format(recipient=recipient_id, text=response["text"]))
+
+    params = {
+        "access_token": FacebookConfig.PAGE_ACCESS_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": response
+    })
+    r = requests.post(FacebookConfig.FACEBOOK_MESSAGEING_API, params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log.info(r.status_code)
+        log.info(r.text)
