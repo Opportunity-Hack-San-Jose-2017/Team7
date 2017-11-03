f95fed604e5abeb00f1420431f818cc1210dd075
Tran Tu <tratu@cisco.com>
2017-11-03 02:12:37 -0700

    add notes on how to post values

--- platobot/platobot/surveys/facebook_survey_manager.py
+++ platobot/platobot/surveys/facebook_survey_manager.py
@@ -1,13 +1,11 @@
 import yaml
 import os
 import logging
-
 from platobot.surveys.survey_manager import SurveyManager
 from platobot.utils.facebook_api import send_response
 from platobot.ushahidi import ushahidi_http, ds
 
 log = logging.getLogger(__name__)
-
 
 class FacebookSurveyManager(SurveyManager):
     def __init__(self):
@@ -36,19 +34,33 @@ class FacebookSurveyManager(SurveyManager):
         except IndexError:
             survey_record.state = -1
             response = self.complete_msg
-            self._survey_complete_hook()
+            self._survey_complete_hook(survey_record.user)
         finally:
             send_response(survey_record.user, response)
 
-    def _survey_complete_hook(self):
+    def _survey_complete_hook(self, user):
         """
         This hook is called after survery is completed
         :return:
         """
+        # session = db_interface.new_session()
+        # record = session.query(models.Survey).filter(
+        #     models.Survey.user == user,
+        #     models.Survey.state != -1
+        #     ).order_by(sqlalchemy.desc(models.Survey.message_submission_time)).first()
+        # session.close()
+        # print(record)
 
+        # notes, the "key" need to be used in values
+        # Location
+        # key: 448ed837-eecb-4306-967e-b394540ea863
+        # How urgent is the situation?
+        # key: 4f48b8c4-7615-405d-9113-1844371ba01e
         post = ds.Post(title='Violent cats - sms edition', content='We are just testing this survey',
-                       # this doesn't work
-                       #values={'813e3cae-02c4-45c3-aa54-05d781ebf113': ["this is what happened"]},
+                       values={
+                           '448ed837-eecb-4306-967e-b394540ea863': [{"lat": "-11.7014801", "lon": "27.4809511429148"}],
+                           '4f48b8c4-7615-405d-9113-1844371ba01e': ["Not Urgent"]
+                        },
                        status='published')
         post.set_form(self.form)
         self.ushahidi_client.save_post(post)
